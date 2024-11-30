86b1d0e31a31e0ca9312ab73952fbf98c0748155
wasilewskyy <jakubwasilewski84@gmail.com>
2024-11-30 09:28:28 +0100

    zadaniaKlasy - classes tasks

---
+++ src/zadaniaKlasy/Kalkulator.java
@@ -0,0 +1,11 @@
+package zadaniaKlasy;
+
+public class Kalkulator {
+    public int obliczSume(int a, int b) {
+        return dodaj(a, b);
+    }
+
+    private int dodaj(int a, int b) {
+        return a + b;
+    }
+}
